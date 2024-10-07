01198b1c387cad99291abac095980d3b771faac6
bitlemon <136308066+bitbitlemon@users.noreply.github.com>
2024-10-07 10:51:38 +0800

    Update resnet_adam.py

--- CIFAR-10/resnet_adam.py
+++ CIFAR-10/resnet_adam.py
@@ -8,7 +8,7 @@ import numpy as np
 import random
 from torchvision.models import resnet18
 
-# 设置随机种子
+
 def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
@@ -20,12 +20,12 @@ def set_seed(seed):
 
 set_seed(42)
 
-# 数据预处理
+
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-# 加载 CIFAR-10 数据集
+
 trainset = torchvision.datasets.CIFAR10(root='./', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=100, shuffle=True, num_workers=2)
 
@@ -34,15 +34,15 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False,
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-# 定义 ResNet-18 模型
-net = resnet18(pretrained=False)
-net.fc = nn.Linear(net.fc.in_features, 10)  # 修改最后一层以适应 CIFAR-10 数据集
 
-# 定义损失函数和优化器
+net = resnet18(pretrained=False)
+net.fc = nn.Linear(net.fc.in_features, 10)  
+
+
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-# 定义 IOU 计算函数
+
 def calculate_iou(outputs, labels):
     _, predicted = torch.max(outputs.data, 1)
     intersection = (predicted == labels).float().sum()
@@ -50,17 +50,17 @@ def calculate_iou(outputs, labels):
     iou = intersection / union
     return iou.item()
 
-# 打开文件以保存指标
+
 with open("resnet18-cifar-adam.txt", "w") as f:
-    # 训练模型
-    for epoch in range(100):  # 训练多个 epoch
+   
+    for epoch in range(100):  
         running_loss = 0.0
         running_iou = 0.0
         for i, data in enumerate(trainloader, 0):
-            # 获取输入数据
+           
             inputs, labels = data
 
-            # 零梯度
+            
             optimizer.zero_grad()
             outputs = net(inputs)
             loss = criterion(outputs, labels)
@@ -69,13 +69,13 @@ with open("resnet18-cifar-adam.txt", "w") as f:
 
             running_loss += loss.item()
 
-            # 计算每个批次的 IOU
+           
             with torch.no_grad():
                 outputs = net(inputs)
                 iou = calculate_iou(outputs, labels)
                 running_iou += iou
 
-        # 计算并保存每个 epoch 的平均损失和 IOU
+        
         epoch_loss = running_loss / len(trainloader)
         epoch_iou = running_iou / len(trainloader)
         f.write(f'Epoch: {epoch + 1}, Average Loss: {epoch_loss:.6f}, Average IOU: {epoch_iou:.4f}\n')
